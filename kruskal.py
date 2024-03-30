b69c1809ab325b06792107a124e92dc6e257efa7
Rafael Takeguma <rafael08.0807@gmail.com>
2024-03-30 19:20:08 -0300

    create function kruskal()

--- kruskal.py
+++ kruskal.py
@@ -19,3 +19,32 @@ class Graph:
         else:
             parent[root_y] = root_x
             rank[root_x] += 1
+
+def kruskal(self):
+    minimum_spanning_tree = []
+    index, edges = 0, 0
+
+    sorted_edges = sorted(self.graph, key=lambda item: item[2])
+
+    parent = []
+    rank = []
+
+    for node in range(self.V):
+        parent.append(node)
+        rank.append(0)
+
+    while edges < self.V - 1:
+        source, destination, weight = sorted_edges[index]
+        index += 1
+
+        root_source = self.find(parent, source)
+        root_destination = self.find(parent, destination)
+
+        if root_source != root_destination:
+            edges += 1
+            minimum_spanning_tree.append([source, destination, weight])
+            self.union(parent, rank, root_source, root_destination)
+
+    print("Edges of the minimum spanning tree")
+    for source, destination, weight in minimum_spanning_tree:
+        print("%d - %d: %d" % (source, destination, weight))
